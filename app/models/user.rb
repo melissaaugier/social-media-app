b9493e508a53139533a63710e5c71c494c82b55d
Mélissa Augier <augiermelissa@gmail.com>
2024-07-31 11:22:15 +0800

    fix devise registration and session

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ApplicationRecord
 
     validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
     validates :password, presence: true, length: { minimum: 6 }
+    validates :password_confirmation, presence: true, length: { minimum: 6 }
+    validates :name, presence: true
+    validates :last_name, presence: true
 end
